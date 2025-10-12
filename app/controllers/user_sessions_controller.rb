6c3c916881ed97f1c37ecaf39202c19f2d6a5202
tsukasa <g.tsukasa.1201@gmail.com>
2025-10-12 16:49:53 +0900

    修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,12 +7,13 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path, success: t('user_sessions.create.success')
-        else
-      flash.now[:danger] = t('user_sessions.create.failure')
+      redirect_to root_path, success: 'ログインしました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
+
 
   def destroy
     logout
