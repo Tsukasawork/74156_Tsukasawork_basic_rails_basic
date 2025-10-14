53ed7c2d329a4f6b5a0fabbd62ee7b20ef4b712d
tsukasa <g.tsukasa.1201@gmail.com>
2025-10-15 07:28:17 +0900

    新規

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_10_13_221012) do
+ActiveRecord::Schema[7.0].define(version: 2025_10_14_220042) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
