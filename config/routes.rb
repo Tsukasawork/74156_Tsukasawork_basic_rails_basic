832faceb1f346c2f5ab6c21d204718dac035abd6
tsukasa <g.tsukasa.1201@gmail.com>
2025-10-14 17:38:25 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create] 
   resources :boards, only: %i[index new]
   resources :boards, only: %i[index]
+  resources :boards, only: %i[index new create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
